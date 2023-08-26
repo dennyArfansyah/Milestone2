b9cd599edf8ef6b4ae58fbe37a630f155705789e
dennyArfansyah <dennyarfansyah@gmail.com>
2023-08-26 17:11:22 +0700

    Initial Commit

---
+++ Milestone2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Milestone2
+//
+//  Created by Denny Arfansyah on 26/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Milestone2/Milestone2App.swift
@@ -0,0 +1,17 @@
+//
+//  Milestone2App.swift
+//  Milestone2
+//
+//  Created by Denny Arfansyah on 26/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Milestone2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
